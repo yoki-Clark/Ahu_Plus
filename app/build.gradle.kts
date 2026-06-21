ae017074c4278956d0f9da46b5c4440fb12f6087
yao-enqi <74yoki@gmail.com>
2026-06-21 22:29:17 +0800

    chore: bump version to 1.1.3 (versionCode 4)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.yourname.ahu_plus"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.1.2"
+        versionCode = 4
+        versionName = "1.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
