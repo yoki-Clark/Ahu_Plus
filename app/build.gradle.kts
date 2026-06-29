1dc1ed3eadcda587ccc0fea04f90c2e32882d933
yao-enqi <74yoki@gmail.com>
2026-06-29 22:34:09 +0800

    chore: 升 v2.2.1.2 (versionCode 23)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.yourname.ahu_plus"
         minSdk = 24
         targetSdk = 36
-        versionCode = 22
-        versionName = "2.2.1.1"
+        versionCode = 23
+        versionName = "2.2.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
