86d8702832b8dc39018477eef771d34c3d096db5
yao-enqi <74yoki@gmail.com>
2026-07-09 17:59:20 +0800

    chore(release): 升级版本号至 2.2.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.yourname.ahu_plus"
         minSdk = 24
         targetSdk = 36
-        versionCode = 24
-        versionName = "2.2.2"
+        versionCode = 25
+        versionName = "2.2.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
